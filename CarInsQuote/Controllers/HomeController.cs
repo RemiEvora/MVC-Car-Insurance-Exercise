71b28553386ba459fd088f290b512482e78a43e2
Revora313 <39279111+Revora313@users.noreply.github.com>
2018-10-02 12:07:29 -0500

    Add project files.

---
+++ CarInsQuote/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsQuote.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CalculateQuote(string firstName, string lastName, string emailAddress,
+                                    int dateOfBirth, int carYear, string carMake, string carModel,
+                                    string dUI, int speedingTicket, string coverageType)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) ||
+                dateOfBirth <= 1900 || carYear <= 1885 || string.IsNullOrEmpty(carModel) || speedingTicket < 0)
+                {
+                return View("~/Views/Shared/error.cshtml");
+                }
+            else
+            {
+                string connectionString = @"Data Source=YUI-LT14\TECHACSQLEXPRESS;Initial Catalog=CarInsurance;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+                string queryString = @"INSERT INTO Insurees (FirstName, LastName, EmailAddress, DateOfBirth,
+                                                             CarYear, CarMake, CarModel, DUI, SpeedingTicket, 
+                                                             CoverageType) VALUES 
+                                                            (@FirstName, @LastName, @EmailAddress, @DateOfBirth,
+                                                             @CarYear, @CarMake, @CarModel, @DUI, @SpeedingTicket, 
+                                                             @CoverageType)";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
+                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
+                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+                    command.Parameters.Add("@DateOfBirth", SqlDbType.Int);
+                    command.Parameters.Add("@CarYear", SqlDbType.Int);
+                    command.Parameters.Add("@CarMake", SqlDbType.VarChar);
+                    command.Parameters.Add("@CarModel", SqlDbType.VarChar);
+                    command.Parameters.Add("@DUI", SqlDbType.VarChar);
+                    command.Parameters.Add("@SpeedingTicket", SqlDbType.Int);
+                    command.Parameters.Add("@CoverageType", SqlDbType.VarChar);
+
+                    command.Parameters["@FirstName"].Value = firstName;
+                    command.Parameters["@LastName"].Value = lastName;
+                    command.Parameters["@EmailAddress"].Value = emailAddress;
+                    command.Parameters["@DateOfBirth"].Value = dateOfBirth;
+                    command.Parameters["@CarYear"].Value = carYear;
+                    command.Parameters["@CarMake"].Value = carMake;
+                    command.Parameters["@CarModel"].Value = carModel;
+                    command.Parameters["@DUI"].Value = dUI;
+                    command.Parameters["@SpeedingTicket"].Value = speedingTicket;
+                    command.Parameters["@CoverageType"].Value = coverageType;
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+                return View("QuotationView");
+            }
+            //USE PIZZA CALCULATION ON GITHUB, PUT IN MODELS FOLDER! 
+            //Create calculate()/CalculateQuote() method, link to Index.cshtml 
+            
+
+
+
+                //Revise SQL Table to include Grand Total field, for Admin usage 
+        }
+        
+    }
+}
---
+++ CarInsQuote/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CarInsQuote")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CarInsQuote")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("69bb6462-cb76-4cf2-a170-1fe21a9a556f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
