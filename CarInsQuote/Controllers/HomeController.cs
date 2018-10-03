5eb0af9a0b6b4998ec7077e7661c14a46ec35801
Revora313 <39279111+Revora313@users.noreply.github.com>
2018-10-03 11:47:03 -0500

    Adding code for review to HomeController.cs and altering values in Insuree.cs to match data types for all related components.

--- CarInsQuote/Controllers/HomeController.cs
+++ CarInsQuote/Controllers/HomeController.cs
@@ -20,35 +20,148 @@ namespace CarInsQuote.Controllers
 
         [HttpPost]
         public ActionResult CalculateQuote(string firstName, string lastName, string emailAddress,
-                                    int dateOfBirth, int carYear, string carMake, string carModel,
-                                    string dUI, int speedingTicket, string coverageType)
+                                    DateTime dateOfBirth, int carYear, string carMake, string carModel,
+                                    string dUI, int speedingTicket, string coverageType, double quote)
         {
             if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) ||
-                dateOfBirth <= 1900 || carYear <= 1885 || string.IsNullOrEmpty(carModel) || speedingTicket < 0)
+                carYear <= 1885 || string.IsNullOrEmpty(carModel) || speedingTicket < 0)
                 {
                 return View("~/Views/Shared/error.cshtml");
                 }
             else
             {
+
+                //==========================================================================================//
+                //QUESTIONS FOR INSTRUCTORS// 
+                //1) Would it be better to encapsulate all of the quote-variable-calculating code 
+                //   into a separate method someplace else and then called in the home controller? 
+                //
+                //2) Can the code function as-written without making them into separate-but-linked 
+                //   methods like the HTML/Javascript Online Pizza exercise (see github 'pizza_functions' link in e-mail for example)?
+                //   The code as it is now does not have anything calling it, simply lines of calculations within 
+                //   the else-statement. Does it need () methods? 
+                // 
+                //3) Similar to #2, I need this if/else statement to function as follows -> 
+                // - Customer enters information 
+                // - If there are any issues, go to the Error Screen 
+                // - If all is well, calculate quote variable AND post all user input + quote calculation to DB. 
+                //
+                // Will this code I've written do this, or is it on the way to doing this? If not, what am I missing, 
+                // or how can I structure it to do so? What do I need to write/add to link it to the add/POST statement 
+                // following the quote-value calculation? 
+
+                {
+
+                    //function ageCheck()
+                    var date1 = DateTime.Now.Year;
+                    var DOB = dateOfBirth.Year;
+                    var baseCost = 50;
+                    var ageFactor = 0;
+                    
+
+                    var dateCheck = date1 - DOB;
+                    if (dateCheck < 18)
+                    {
+                        ageFactor = 100;
+                    }
+                    else if (dateCheck > 18 && dateCheck < 25)
+                    {
+                        ageFactor = 25; 
+                    }
+                    else if (dateCheck > 100)
+                    {
+                        ageFactor = 100; 
+                    }
+   
+
+                    //function carYearCheck()
+                    var carYearFactor = 0; 
+                    if (carYear < 2000 || carYear > 2015)
+                    {
+                        carYearFactor = 25;
+                    }
+                    else
+                    {
+                        carYearFactor = 0; 
+                    }
+
+
+                    //function carMakeCheck()
+                    var isPorsche = carMake.ToLower();
+                    var carMakeFactor = 0;
+                    if (isPorsche == "porsche")
+                    {
+                        carMakeFactor = 25; 
+                    }
+                    else
+                    {
+                        carMakeFactor = 0;
+                    }
+
+
+                    //function carModelCheck()
+                    var isCarerra = carModel.ToLower();
+                    var carModelFactor = 0;
+                    if (carModel == "carrera 911" || carModel == "carrera911" || carModel == "911 carrera" || carModel == "911carrera")
+                    {
+                        carModelFactor = 25; 
+                    }
+                    else
+                    {
+                        carModelFactor = 0; 
+                    }
+
+                   
+                    //function speedingTicketCheck() 
+                    var speedingTicketFactor = speedingTicket * 10;
+
+                    //TOTAL BEFORE PERCENTAGES 
+                    double runningTotal = baseCost + ageFactor + carYearFactor + carMakeFactor + carModelFactor + speedingTicketFactor;
+
+                    //function dUICheck(runningTotal)
+                    double dUIFactor = 0; 
+                    if (dUI == "Yes")
+                    {
+                        dUIFactor = runningTotal * 0.25; 
+                    }
+                    else
+                    {
+                        dUIFactor = 0; 
+                    }
+
+                    double runningTotal2 = runningTotal + dUIFactor;
+
+                    //function coverageCheck(runningTotal2)
+                    double coverageFactor = 0; 
+                    if (coverageType == "Full")
+                    {
+                        coverageFactor = runningTotal2 * 0.50;
+                    }
+
+                    double grandTotal = runningTotal2 + coverageFactor; 
+                }
+//==========================================================================================//
+
                 string queryString = @"INSERT INTO Insurees (FirstName, LastName, EmailAddress, DateOfBirth,
                                                              CarYear, CarMake, CarModel, DUI, SpeedingTicket, 
-                                                             CoverageType) VALUES 
+                                                             CoverageType, Quote) VALUES 
                                                             (@FirstName, @LastName, @EmailAddress, @DateOfBirth,
                                                              @CarYear, @CarMake, @CarModel, @DUI, @SpeedingTicket, 
-                                                             @CoverageType)";
+                                                             @CoverageType, @Quote)";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     SqlCommand command = new SqlCommand(queryString, connection);
                     command.Parameters.Add("@FirstName", SqlDbType.VarChar);
                     command.Parameters.Add("@LastName", SqlDbType.VarChar);
                     command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
-                    command.Parameters.Add("@DateOfBirth", SqlDbType.Int);
+                    command.Parameters.Add("@DateOfBirth", SqlDbType.DateTime);
                     command.Parameters.Add("@CarYear", SqlDbType.Int);
                     command.Parameters.Add("@CarMake", SqlDbType.VarChar);
                     command.Parameters.Add("@CarModel", SqlDbType.VarChar);
                     command.Parameters.Add("@DUI", SqlDbType.VarChar);
                     command.Parameters.Add("@SpeedingTicket", SqlDbType.Int);
                     command.Parameters.Add("@CoverageType", SqlDbType.VarChar);
+                    command.Parameters.Add("@Quote", SqlDbType.Decimal);
 
                     command.Parameters["@FirstName"].Value = firstName;
                     command.Parameters["@LastName"].Value = lastName;
@@ -60,18 +173,19 @@ namespace CarInsQuote.Controllers
                     command.Parameters["@DUI"].Value = dUI;
                     command.Parameters["@SpeedingTicket"].Value = speedingTicket;
                     command.Parameters["@CoverageType"].Value = coverageType;
+                    command.Parameters["@Quote"].Value = quote; 
 
                     connection.Open();
                     command.ExecuteNonQuery();
                     connection.Close();
                 }
-                return View("QuotationView"); //Not working at the moment due to not having any Quote value entered.
+                return View("QuotationView"); 
             }
         }
         public ActionResult Admin()
         {
             string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, DateOfBirth,
-                                   CarYear, CarMake, CarModel, DUI, SpeedingTicket, CoverageType FROM Insurees";
+                                   CarYear, CarMake, CarModel, DUI, SpeedingTicket, CoverageType, Quote FROM Insurees";
             List<Insuree> quotelist = new List<Insuree>();
 
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -89,7 +203,7 @@ namespace CarInsQuote.Controllers
                     customer.FirstName = reader["FirstName"].ToString();
                     customer.LastName = reader["LastName"].ToString();
                     customer.EmailAddress = reader["EmailAddress"].ToString();
-                    customer.DateOfBirth = Convert.ToInt32(reader["DateOfBirth"]);
+                    customer.DateOfBirth = Convert.ToDateTime(reader["DateOfBirth"]);
                     customer.CarYear = Convert.ToInt32(reader["CarYear"]);
                     customer.CarMake = reader["CarMake"].ToString();
                     customer.CarModel = reader["CarModel"].ToString();
@@ -102,7 +216,6 @@ namespace CarInsQuote.Controllers
             }
                 return View(quotelist);
         }
-        //USE PIZZA CALCULATION ON GITHUB, PUT IN MODELS FOLDER! 
-        //Create calculate()/CalculateQuote() method, link to Index.cshtml 
+       
     }
 }
--- CarInsQuote/Models/Insuree.cs
+++ CarInsQuote/Models/Insuree.cs
@@ -11,7 +11,7 @@ namespace CarInsQuote.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
-        public int DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public int CarYear { get; set; }
         public string CarMake { get; set; }
         public string CarModel { get; set; }
